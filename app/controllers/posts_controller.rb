8a821d717c06e51f1041d0c2bff16590e960c424
Iddahodhiambo <iddahawuor@yahoo.com>
2023-05-07 11:36:00 +0300

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,17 +7,21 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post= find_post
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
 
   def post_params
     params.permit(:category, :content, :title)
+
+  end
+  def find_post
+    Post.find_by(id: params[:id])
   end
 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+
+    validates :email, uniqueness: true
+    validates :name,   presence: true
 end
